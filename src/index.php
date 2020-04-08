2bbcbc722bfb32bdc02809fea8a5503667af5fff
Shuji Yamagami <yamagami@a-livesystems.com>
2020-04-09 00:17:53 +0900

    sec1 pc

--- src/index.php
+++ src/index.php
@@ -7,7 +7,7 @@ include($path.'libs/meta.php');
 </head>
 
 <body class="top">
-
+<div id="fixbnr"><a href="#"><img src="./images/common/fixed_bnr.png" alt=""></a></div>
 
 
 <!-- Header
@@ -26,7 +26,28 @@ include($path.'libs/meta.php');
 	======================================================================-->
 	<main>
 
+		<section id="main">
+			<div id="main__image" style="background-image: url(./images/top/main.jpg);">
+				<div id="main__cap"><img src="./images/top/main_cap.png" alt=""></div>
+				<div id="main__scroll">
+					<a href="#">SCROLL</a>
+				</div>
+			</div>
+		</section>
 
+		<section id="sec01">
+			<div class="inner">
+				<div class="flex-row">
+					<div>
+						<a href="#">開催概要</a>
+					</div>
+					<div>
+						<a href="#">募集要項</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		
 	</main>
 
 
