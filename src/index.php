52a02a5d7368a13c65ad47f14ada4cf585a0229c
Shuji Yamagami <yamagami@a-livesystems.com>
2020-04-09 08:28:29 +0900

    top pc done

--- src/index.php
+++ src/index.php
@@ -47,7 +47,101 @@ include($path.'libs/meta.php');
 				</div>
 			</div>
 		</section>
+
+
+		<section id="sec02">
+			<div class="inner">
+				<h2 class="title">NEWS</h2>
+				<ul>
+					<li>2020.01.01</li>
+					<li><a href="">ニュース記事のタイトルがこちらにはいります。</a></li>
+				</ul>
+				<ul>
+					<li>2020.01.01</li>
+					<li><a href="">ニュース記事のタイトルがこちらにはいります。</a></li>
+				</ul>
+				<ul>
+					<li>2020.01.01</li>
+					<li><a href="">ニュース記事のタイトルがこちらにはいります。</a></li>
+				</ul>
+				<a href="">MORE</a>
+			</div>
+		</section>
+
+
+		<section id="sec03">
+			<div class="inner">
+				<h2 class="title">ABOUT <a href="">MORE</a></h2>
+				<p>アバウト詳細に誘導するコメントが入ります。アバウト詳細に誘導するコメントが入ります。アバウト詳細に誘導するコメントが入ります。アバウト詳細に誘導するコメントが入ります。アバウト詳細に誘導するコメントが入ります。アバウト詳細に誘導するコメントが入ります。アバウト詳細に誘導するコメントが入ります。アバウト詳細に誘導するコメントが入ります。アバウト詳細に誘導するコメントが入ります。アバウト詳細に誘導するコメントが入ります。アバウト詳細に誘導するコメントが入ります。アバウト詳細に誘導するコメントが入ります。アバウト詳細に誘導するコメントが入ります。アバウト詳細に誘導するコメントが入ります。アバウト詳細に誘導するコメントが入ります。アバウト詳細に誘導するコメントが入ります。アバウト詳細に誘導するコメントが入ります。</p>
+			</div>
+		</section>
 		
+
+		<section id="sec04">
+			<div class="inner">
+				<h2 class="title">ARTIST <a href="">MORE</a></h2>
+			</div>
+			<div class="infiniteslide">
+				<div><a href=""><img src="./images/top/artist01.png" alt="" /></a></div>
+				<div><a href=""><img src="./images/top/artist01.png" alt="" /></a></div>
+				<div><a href=""><img src="./images/top/artist01.png" alt="" /></a></div>
+				<div><a href=""><img src="./images/top/artist01.png" alt="" /></a></div>
+				<div><a href=""><img src="./images/top/artist01.png" alt="" /></a></div>
+				<div><a href=""><img src="./images/top/artist01.png" alt="" /></a></div>
+				<div><a href=""><img src="./images/top/artist01.png" alt="" /></a></div>
+				<div><a href=""><img src="./images/top/artist01.png" alt="" /></a></div>
+				<div><a href=""><img src="./images/top/artist01.png" alt="" /></a></div>
+			</div>
+			<p>アーティスト一覧に誘導するコメントが入ります。アーティスト一覧に誘導するコメントが入ります。アーティスト一覧に誘導するコメントが入ります。アーティスト一覧に誘導するコメントが入ります。アーティスト一覧に誘導するコメントが入ります。アーティスト一覧に誘導するコメントが入ります。アーティスト一覧に誘導するコメントが入ります。アーティスト一覧に誘導するコメントが入ります。アーティスト一覧に誘導するコメントが入ります。アーティスト一覧に誘導するコメントが入ります。</p>
+		</section>
+
+
+		<section id="sec05">
+			<div class="inner">
+				<h2 class="title">EVENT <a href="#">MORE</a></h2>
+				<p>イベント一覧に誘導するコメントが入ります。イベント一覧に誘導するコメントが入ります。イベント一覧に誘導するコメントが入ります。イベント一覧に誘導するコメントが入ります。イベント一覧に誘導するコメントが入ります。イベント一覧に誘導するコメントが入ります。イベント一覧に誘導するコメントが入ります。</p>
+				<div class="flex-row">
+					<a href="">
+						<figure>
+							<div></div>
+							<figcaption>
+								<div><span>音楽</span><em>2020.01.04</em></div>
+								<p>記事のタイトルがこちらに入ります。<br>２行までで抑えます。</p>
+							</figcaption>
+						</figure>
+					</a>
+					<a href="">
+						<figure>
+							<div></div>
+							<figcaption>
+								<div><span>音楽</span><em>2020.01.04</em></div>
+								<p>記事のタイトルがこちらに入ります。<br>２行までで抑えます。</p>
+							</figcaption>
+						</figure>
+					</a>
+					<a href="">
+						<figure>
+							<div></div>
+							<figcaption>
+								<div><span>音楽</span><em>2020.01.04</em></div>
+								<p>記事のタイトルがこちらに入ります。<br>２行までで抑えます。</p>
+							</figcaption>
+						</figure>
+					</a>
+				</div>
+			</div>
+		</section>
+
+
+		<section id="sec06">
+			<div class="inner">
+				<div>
+					<h3>REPORTS</h3>
+					<p>過去のイベント一覧に誘導するコメントが入ります。過去のイベント一覧に誘導するコメントが入ります。過去のイベント一覧に誘導するコメントが入ります。過去のイベント一覧に誘導するコメントが入ります。過去のイベント一覧に誘導するコメントが入ります。過去のイベント一覧に誘導するコメントが入ります。過去のイベント一覧に誘導するコメントが入ります。過去のイベント一覧に誘導するコメントが入ります。過去のイベント一覧に誘導するコメントが入ります。過去のイベント一覧に誘導するコメントが入ります。過去のイベント一覧に誘導するコメントが入ります。過去のイベント一覧に誘導するコメントが入ります。過去のイベント一覧に誘導するコメントが入ります。</p>
+					<a href="#">過去のアートフェスの様子を見る</a>
+				</div>
+			</div>
+		</section>
 	</main>
 
 
@@ -65,5 +159,16 @@ include($path.'libs/meta.php');
 <!-- Scripts
 ======================================================================-->
 <?php include($path.'libs/scripts.php'); ?>
+<script src="./js/infinity.min.js"></script>
+<script>
+$(function(){
+	$('.infiniteslide').infiniteslide({
+		'speed': 50,
+		'direction': 'left', //choose  up/down/left/right
+		'pauseonhover': false, //if true,stop onmouseover
+		'responsive': false, //width/height recalculation on window resize. child element's width/height define %/vw/vh,this set true.
+	});
+});
+</script>
 </body>
 </html>
