d33698ba6f8bb505aa2fb40bfad7549a84a1f511
Shuji Yamagami <yamagami@a-livesystems.com>
2020-05-01 13:50:25 +0900

    mod

--- src/libs/footer.php
+++ src/libs/footer.php
@@ -2,12 +2,12 @@
   <div class="inner">
     <div class="flex-row">
       <div class="footer__map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13133.389326658209!2d135.4983715!3d34.620662!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x73ab58ee71ce3a2a!2z5a2m5qCh5rOV5Lq6IOW4neWhmuWxseWtpumZog!5e0!3m2!1sja!2sjp!4v1586357139619!5m2!1sja!2sjp" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13121.399096530313!2d135.7498515!3d34.6963568!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x59a19f813b278ff1!2z5bid5aGa5bGx5aSn5a2mIOWliOiJr-ODu-WtpuWckuWJjeOCreODo-ODs-ODkeOCuQ!5e0!3m2!1sja!2sjp!4v1588230980925!5m2!1sja!2sjp" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
       </div>
       <div class="footer__access">
         <p>
-          学園前街育プロジェクト実行委員会 事務局（学校法人帝塚山学園 内）<br>
-          TEL : 0742-41-4509　FAX : 0742-48-9894
+          学園前街育プロジェクト実行委員会 事務局（学校法人帝&#xfa10;山学園 内）<br>
+          TEL : 0742-41-4659　FAX : 0742-48-9894
         </p>
         <a href="<?php echo APP_URL; ?>contact/">
           <img src="<?php echo APP_URL; ?>images/common/mail.png" alt="">
@@ -38,12 +38,12 @@
 
     <div class="footer__supporter">
       <div class="flex-row">
+      <a href="" target="_blank"><img src="<?php echo APP_URL; ?>images/common/gakuenminami_bn.png" alt=""></a>
         <a href="https://tezukayamagakuen.jp/" target="_blank"><img src="<?php echo APP_URL; ?>images/common/tezuka2.png" alt=""></a>
-        <a href="" target="_blank"><img src="<?php echo APP_URL; ?>images/common/gakuenminami_bn.png" alt=""></a>
       </div>
     </div>
     <div class="footer__copy">
-      <p>&copy; 2015-2020 GAKUENMAE ART FESTA. All rights reserved.</p>
+      <p>&copy; GAKUENMAE ART FESTA. All rights reserved.</p>
     </div>
   </div>
 </footer>
